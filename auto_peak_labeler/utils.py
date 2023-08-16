07b689e462bed60c010e9f78b5bd6737f530bc0a
carbonscott <wangimagine@gmail.com>
2023-08-15 17:12:55 -0700

    A __draft__ version of padding in generating patch_list.

--- auto_peak_labeler/utils.py
+++ auto_peak_labeler/utils.py
@@ -20,6 +20,14 @@ def get_patch_list(peaks_y, peaks_x, img, win_size):
         # Both variables are views of the original data
         img_peak = img[y_min:y_max, x_min:x_max]
 
+        # ...Padding
+        x_pad_l = -min(x - win_size, 0)            # ...Lower
+        x_pad_u =  max(x + win_size + 1 - W, 0)    # ...Upper
+        y_pad_l = -min(y - win_size, 0)            # ...Lower
+        y_pad_u =  max(y + win_size + 1 - H, 0)    # ...Upper
+        padding = ((y_pad_l, y_pad_u), (x_pad_l, x_pad_u))
+        img_peak = np.pad(img_peak, padding, mode='constant', constant_values=0)
+
         patch_list.append(img_peak)
 
     return patch_list
